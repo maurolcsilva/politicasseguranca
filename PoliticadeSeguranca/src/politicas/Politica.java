559c7bf8cd4f7ba9753b64a845855bc60608e63d
maurolcsilva <maurolcsilva@gmail.com>
2013-05-10 17:45:45 -0300

    Atualizacao das Classes

--- PoliticadeSeguranca/src/politicas/Politica.java
+++ PoliticadeSeguranca/src/politicas/Politica.java
@@ -1,5 +1,20 @@
 package politicas;
 
 public class Politica {
+	private String definicao;
+	private String pessoas;
+	
+	public String getDefinicao() {
+		return definicao;
+	}
+	public void setDefinicao(String definicao) {
+		this.definicao = definicao;
+	}
+	public String getPessoas() {
+		return pessoas;
+	}
+	public void setPessoas(String pessoas) {
+		this.pessoas = pessoas;
+	}
 
 }
